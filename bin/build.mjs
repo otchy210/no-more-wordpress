9ab014d38d1381432cbb32b6bb2f60a39f305cac
Otchy <otchy@localhost>
2021-01-08 23:43:25 +0900

    Implemnet blog list builder

--- bin/build.mjs
+++ bin/build.mjs
@@ -8,28 +8,37 @@ const DOCS_ROOT = path.resolve(config.dirs.docs);
 
 const main = async () => {
     await fs.mkdir(DOCS_ROOT, {recursive: true});
-    await handleDir([]);
+    const posts = [];
+    await handleDir([], posts);
+    const blogPosts = posts
+        .filter(post => post.type === 'post')
+        .sort((left, right) => right.time - left.time);
+    await buildBlogLists(blogPosts);
 };
 
-const handleDir = async (dirPaths) => {
-    handlePost(dirPaths);
+const handleDir = async (dirPaths, posts) => {
+    const post = await handlePost(dirPaths);
+    if (post) {
+        posts.push(post);
+    }
     const currDir = `${DATA_ROOT}/${dirPaths.join('/')}`;
     const paths = await fs.readdir(currDir);
     for (const path of paths) {
         const nextPath = `${currDir}/${path}`;
         const stat = await fs.lstat(nextPath);
         if (stat.isDirectory()) {
-            handleDir([...dirPaths, path]);
+            await handleDir([...dirPaths, path], posts);
         }
     }
 };
 
 const handlePost = async (dirPaths) => {
-    const dataDir = `${DATA_ROOT}/${dirPaths.join('/')}`;
+    const path = dirPaths.join('/');
+    const dataDir = `${DATA_ROOT}/${path}`;
     const contentPath = `${dataDir}/content.html`;
     const stat = await fs.lstat(contentPath).catch(() => {/* ignore */});
     if (!stat || !stat.isFile()) {
-        return;
+        return false;
     }
     const meta = await getPostMeta(dataDir);
     const content = await fs.readFile(contentPath, 'utf-8');
@@ -39,12 +48,17 @@ const handlePost = async (dirPaths) => {
         title,
         body
     };
-    const html = template(post);
+    const html = template.page(post);
 
     const htmlDir = `${DOCS_ROOT}/${dirPaths.join('/')}`;
     await fs.mkdir(htmlDir, {recursive: true});
     const htmlPath = `${htmlDir}/index.html`;
-    fs.writeFile(htmlPath, html);
+    await fs.writeFile(htmlPath, html);
+    return {
+        ...post,
+        path,
+        body: truncate(body)
+    };
 }
 
 const getPostMeta = async (dir) => {
@@ -55,6 +69,42 @@ const getPostMeta = async (dir) => {
     }
     const json = await fs.readFile(path, 'utf-8');
     return JSON.parse(json);
+};
+
+const truncate = (content) => {
+    const noTagContent = content.split(/<[^>]+>/).join(' ').split(/\s+/).join(' ');
+    if (noTagContent.length < 120) {
+        return noTagContent;
+    }
+    return `${noTagContent.substr(0, 120)}…`;
+};
+
+const buildBlogLists = async (blogPosts) => {
+    let offset = 0;
+    while (true) {
+        const articles = [];
+        for (let i = 0; i < 5 ; i++) {
+            const j = offset + i;
+            if (j >= blogPosts.length) {
+                break;
+            }
+            const post = blogPosts[j];
+            const article = template.article(post);
+            articles.push(article);
+        }
+        offset += 5;
+        const page = offset / 5;
+        const html = template.page({
+            body: articles.join('\n')
+        });
+        const htmlDir = page === 1 ? DOCS_ROOT : `${DOCS_ROOT}/page/${page}`;
+        await fs.mkdir(htmlDir, {recursive: true});
+        const htmlPath = `${htmlDir}/index.html`;
+        await fs.writeFile(htmlPath, html);
+        if (offset > blogPosts.length) {
+            break;
+        }
+    }
 };
 
 main().catch(err => {
--- bin/template.mjs
+++ bin/template.mjs
@@ -1,12 +1,30 @@
-export default ({title, body, category, time, tags}) => {
+const page = ({title, body, category, time, tags}) => {
     return `
 <html>
 <head>
-    <title>${title}</title>
+    <title>${title ? `${title} - ` : ''}OTCHY.NET</title>
 </head>
 <body>
-${body}
+    <main>
+        ${body}
+    </main>
 </body>
 </html>
 `;
+};
+
+const article = ({title, body, path}) => {
+    return `
+<article>
+    <h1><a href="/${path}">${title}</a></h1>
+    <p>
+        ${body}
+    </p>
+</article>
+`;
+};
+
+export default {
+    page,
+    article
 };
